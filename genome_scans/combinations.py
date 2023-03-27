3dde242ea94fade03fd9525978714bdb2f4ca646
linsalrob <raedwards@gmail.com>
2023-03-27 17:36:33 +1030

    more sensible name

--- genome_scans/combinations.py
+++ genome_scans/combinations.py
@@ -9,11 +9,14 @@ from itertools import permutations
 
 __author__ = 'Rob Edwards'
 
-file_endings = ['P_aeruginosa.bam', '16S.bam', 'Myco100.bam', 'Staph.bam', 'Strep.bam', 'Pseudomonas_aeruginosa.fasta']
+file_endings = ['P_aeruginosa.bam', '16S.bam', 'Myco100.bam', 'Staph.bam', 'Strep.bam']
 
 with open("IDs.tsv", "r") as ids:
-    for sample in ids:
-        joined = [f"{s}.{x}" for x in file_endings]
+    for line in ids:
+        if line.startswith('ULN'):
+            continue
+        tid, sample = line.strip().split("\t")
+        joined = [f"bam/{sample}.{x}" for x in file_endings]
         for i in range(len(joined)):
             p = joined.pop(0)
             t = " -n ".join(joined)
